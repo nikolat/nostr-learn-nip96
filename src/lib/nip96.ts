7c2dc1f0fe830adaa044a8b39a1265a7f895dde4
Don <496156+nikolat@users.noreply.github.com>
2025-03-03 19:17:24 +0900

    型あるよ

--- src/lib/nip96.ts
+++ src/lib/nip96.ts
@@ -69,8 +69,7 @@ export async function uploadFile(
 export async function listFiles(
 	serverApiUrl: string,
 	nip98AuthorizationHeader: string
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-): Promise<any> {
+): Promise<FileListResponse> {
 	// Send the GET request
 	const response = await fetch(serverApiUrl, {
 		method: 'GET',
